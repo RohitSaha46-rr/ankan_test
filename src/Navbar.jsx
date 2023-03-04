03dfd6c4d118310d2cb9196f2f63eff624c334ec
Rohit <saharohit158@gmail.com>
2023-03-05 00:31:52 +0530

    fuck

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -11,6 +11,7 @@ const Navbar = () => {
               <li>About</li>
               <li>Contact Us</li>
               <li>Auto1</li>
+              <li>jahaj</li>
             </ul>
         </div>
     </div>
